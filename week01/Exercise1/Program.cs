fdd2c7dcf2801f4048299344e7c57a675bea0b58
ZelalemEshetu <zelalemeshetubeshah@gmail.com>
2025-07-11 05:50:44 +0300

    Finished prep 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //ask for first name
+        Console.Write("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        //ask for last name
+        Console.Write("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        //output in the correct format
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
